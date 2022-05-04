50677d3e944e04ca01b6111d86f29d7df958cf1f
Arthur <giarto@mail.ru>
2022-05-04 13:19:07 +0300

    first commit

--- main.go
+++ main.go
@@ -8,16 +8,16 @@ import (
 
 // album для хранения данных альбома в памяти.
 type album struct {
-	ID     int  `json:"id"`
+	ID     int     `json:"id"`
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
 }
 
 var albums = []album{
-	{ID: 1, Title: "Blue train", Artist: "John Coltrane", Price: 56,99}
-	{ID: 2, Title: "Jeru", Artist: "Gerry Mulligan", Price: 17,99}
-	{ID: 3, Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan ", Price: 39,99}
+	{ID: 1, Title: "Blue train", Artist: "John Coltrane", Price: 56.99},
+	{ID: 2, Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: 3, Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan ", Price: 39.99},
 }
 
 // getAlbums возвращает список всех альбомов в формате JSON.
@@ -28,7 +28,28 @@ func getAlbums(c *gin.Context) {
 }
 
 func main() {
+	// Инициализируете роутер Gin, используя Default.
 	router := gin.Default()
+
+	// Используете функцию GET, чтобы связать метод GET HTTP и путь /albums с функцией обработчика.
 	router.GET("/albums", getAlbums)
+	// Связываете метод POST по пути /albums с функцией postAlbums.
+	router.POST("/albums", postAlbums)
+
+	// Используйте функцию Run, чтобы подключить маршрутизатор к http.Server и запустить сервер.
 	router.Run("localhost:8080")
+}
+
+// postAlbums добавляет альбом из JSON, полученного в теле запроса.
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Вызов BindJSON для привязки полученного JSON к newAlbum
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Добавляем в срез новый альбом.
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
