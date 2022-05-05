45f638f143e0c974fb9ea5febf95529d4e30269c
Arthur <giarto@mail.ru>
2022-05-05 20:14:24 +0300

    second commit

---
+++ handlers.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+// getAlbums возвращает список всех альбомов в формате JSON.
+func getAlbums(c *gin.Context) {
+	// Вызовете Context.IndentedJSON, чтобы сериализовать структуру в JSON и добавить ее в ответ.
+	// Первый аргумент функции - это код состояния HTTP, который вы хотите отправить клиенту. Здесь вы передаете константу StatusOK из пакета net/http, чтобы указать 200 OK.
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// postAlbums добавляет альбом из JSON, полученного в теле запроса.
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Вызов BindJSON для привязки полученного JSON к newAlbum
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Добавляем в срез новый альбом.
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// getAlbumByID находит альбом,
+// значение идентификатора которого совпадает с
+// параметром id, отправленным клиентом,
+// затем возвращает этот альбом в качестве ответа
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Перебираем список альбомов в поисках альбома,
+	// значение идентификатора которого соответствует параметру.
+	for _, a := range albums {
+		if s, _ := strconv.Atoi(id); a.ID == s {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
--- main.go
+++ main.go
@@ -1,31 +1,8 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
-
-// album для хранения данных альбома в памяти.
-type album struct {
-	ID     int     `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-var albums = []album{
-	{ID: 1, Title: "Blue train", Artist: "John Coltrane", Price: 56.99},
-	{ID: 2, Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: 3, Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan ", Price: 39.99},
-}
-
-// getAlbums возвращает список всех альбомов в формате JSON.
-func getAlbums(c *gin.Context) {
-	// Вызовете Context.IndentedJSON, чтобы сериализовать структуру в JSON и добавить ее в ответ.
-	// Первый аргумент функции - это код состояния HTTP, который вы хотите отправить клиенту. Здесь вы передаете константу StatusOK из пакета net/http, чтобы указать 200 OK.
-	c.IndentedJSON(http.StatusOK, albums)
-}
 
 func main() {
 	// Инициализируете роутер Gin, используя Default.
@@ -35,39 +12,9 @@ func main() {
 	router.GET("/albums", getAlbums)
 	// Связываете метод POST по пути /albums с функцией postAlbums.
 	router.POST("/albums", postAlbums)
+	// Связываете путь /albums/:id с функцией getAlbumByID. В Gin двоеточие перед элементом в пути означает, что этот элемент является параметром пути.
+	router.GET("/albums/:id", getAlbumByID)
 
 	// Используйте функцию Run, чтобы подключить маршрутизатор к http.Server и запустить сервер.
 	router.Run("localhost:8080")
-}
-
-// postAlbums добавляет альбом из JSON, полученного в теле запроса.
-func postAlbums(c *gin.Context) {
-	var newAlbum album
-
-	// Вызов BindJSON для привязки полученного JSON к newAlbum
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
-	}
-
-	// Добавляем в срез новый альбом.
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
-}
-
-// getAlbumByID находит альбом,
-// значение идентификатора которого совпадает с
-// параметром id, отправленным клиентом,
-// затем возвращает этот альбом в качестве ответа
-func getAlbumByID(c *gin.Context) {
-	id := c.Param("id")
-
-	// Перебираем список альбомов в поисках альбома,
-	// значение идентификатора которого соответствует параметру.
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
-	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
